f0f9b469a17d6bd8592590a895d92f049d3facab
muhammadariefhidayat <tubagusarief45@gmail.com>
2019-05-15 14:14:50 +0700

    Menampilkan Array ke dalam RecyclerView

---
+++ app/src/main/java/org/odoj/kadedicoding/DAftarClubActivity.kt
@@ -0,0 +1,12 @@
+package org.odoj.kadedicoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DAftarClubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_club)
+    }
+}
